232b51e559028acaa4a3021017b38ce474dbccce
Reed Vogt <readvogt@gmail.com>
2023-06-02 16:30:44 -0700

    second

--- eventPool.js
+++ eventPool.js
@@ -3,7 +3,15 @@
 const Events = require('events');
 const eventEmitter = new Events();
 
-const eventPool = ['pickup', 'transit', 'delivered'];
+const eventPool = {
+  PLAYER_JOIN: 'playerJoin',
+  GAME_START: 'gameStart',
+  UPDATE_PLAYER: 'updatePlayer',
+  PLAYER_GUESS: 'playerGuess',
+  PLAYER_SCORE: 'playerScore',
+  NEW_CARD: 'newCard',
+  PLAYER_TURN: 'playerTurn',
+};
 
 module.exports = {
   eventPool,
--- hub.js
+++ hub.js
@@ -1,60 +1,167 @@
 'use strict';
 
+// Load environment variables from .env file
 require('dotenv').config();
+
+// Import chalk for styling console output
+const chalk = require('chalk');
+
+// Import necessary modules
 const { Server } = require('socket.io');
-const PORT = process.env.PORT || 3001;
+const io = new Server(process.env.PORT);
+const PlayerQueue = require('./lib/playerqueue');
+const eventPool = require('./eventPool');
 
-const { eventEmitter, eventPool } = require('./eventPool');
+// Create a player queue and word pool for the game
+let playerQueue = new PlayerQueue();
+let wordPool = [
+  'dark magician',
+  'blue eyes',
+  'buster blader',
+  'kuriboh',
+  'exodia',
+  'slifer',
+  'obelisk',
+  'ra',
+  'summoned skull',
+  'winged kuriboh',
+];
 
-const io = new Server(PORT);
-const capsServer = io.of('/caps');
+// Randomly select a secret word from the word pool
+let secretWord = wordPool[Math.round(Math.random() * (wordPool.length - 1))];
 
-const logEvent = eventName => payload => {
-  console.log(
-    `
-    EVENT: {
-      event: ${eventName},
-      time: ${new Date()},
-      payload:`,
-    payload,
-  );
-};
+// Create an array to represent the revealed word with underscores
+let revealedWord = Array(secretWord.length).fill('_');
 
-capsServer.on('connection', socket => {
-  console.log(`CLIENT CONNECTED TO CAPS SERVER \n SOCKET: `, socket.id);
+// Initialize the turn ID to 1
+let turnId = 1;
 
-  socket.on('join', payload => {
-    socket.join(payload.store);
+// Log the secret word to the console (for debugging purposes)
+console.log(secretWord);
+
+// Event listener for client connections
+io.on('connection', socket => {
+  console.log('CLIENT CONNECTED TO SERVER: ', socket.id);
+
+  // Event handler for the 'PLAYER_JOIN' event
+  socket.on(eventPool.eventPool.PLAYER_JOIN, player => {
+    // Add the player to the queue and get the updated player object
+    let updatedPlayer = playerQueue.addPlayer(player);
+
+    // Emit the updated player object to the joining player
+    io.to(socket.id).emit(eventPool.eventPool.UPDATE_PLAYER, updatedPlayer);
+
+    // Join the 'gameRoom' and log the player's name
+    socket.join('gameRoom');
+    console.log(`${player.name} HAS JOINED THE GAME ROOM`);
+
+    // Emit the 'PLAYER_JOIN' event to the joining player
+    socket.emit(eventPool.eventPool.PLAYER_JOIN, player);
+
+    // Check the number of clients in the room
+    const clientsInRoom = socket.adapter.rooms.get('gameRoom');
+    if (clientsInRoom.size >= 2) {
+      // Prepare the payload for the game start event
+      let payload = {
+        turnId: turnId,
+        revealedWord: revealedWord,
+      };
+
+      // Emit the 'GAME_START' event to all players in the room except the joining player
+      socket.to('gameRoom').emit(eventPool.eventPool.GAME_START, payload);
+    }
   });
 
-  // logs 'PICKUP' event to server console when vendor has package ready for pickup
-  socket.on(eventPool[0], logEvent(eventPool[0]));
+  // Event handler for the 'PLAYER_GUESS' event
+  socket.on(eventPool.eventPool.PLAYER_GUESS, guessLetter => {
+    let addedScore = 0;
 
-  // sends a 'PICKUP' alert to the drivers, letting them know that a package is ready for pickup
-  socket.on(eventPool[0], payload => {
-    console.log('Package ready for pickup; Notifying driver.');
+    // Iterate over the secret word to check for matches with the guessed letter
+    for (let idx = 0; idx < secretWord.length; idx++) {
+      if (guessLetter.toLowerCase() === secretWord[idx].toLowerCase()) {
+        // Update the revealed word and increase the player's score
+        revealedWord[idx] = secretWord[idx];
+        playerQueue.players[turnId - 1].score++;
+        addedScore++;
+      }
+    }
 
-    // capsServer.emit(eventPool[0], payload);
-    socket.broadcast.emit(eventPool[0], payload);
-  });
+    // Prepare the payload for the 'PLAYER_SCORE' event
+    let payload = {
+      addedScore: addedScore,
+      revealedWord: revealedWord,
+    };
 
-  // logs 'IN-TRANSIT' event to server console when driver is en route to delivery address
-  socket.on(eventPool[1], logEvent(eventPool[1]));
+    if (addedScore > 0) {
+      // Emit the 'PLAYER_SCORE' event to the guessing player if they earned points
+      socket.emit(eventPool.eventPool.PLAYER_SCORE, payload);
+    }
 
-  // logs 'DELIVERED' event to server console when driver successfully delivers package
-  socket.on(eventPool[2], logEvent(eventPool[2]));
+    // Check if the guessed letter does not match any letters in the secret word
+    let noLetterMatch = !secretWord.some(
+      letter => letter.toLowerCase() === guessLetter.toLowerCase(),
+    );
 
-  socket.on(eventPool[2], payload => {
-    console.log('Notifying vendor that package was delivered.');
+    if (noLetterMatch) {
+      // Emit a message to the guessing player if there was no match
+      socket.emit(
+        eventPool.eventPool.PLAYER_GUESS,
+        chalk.red(`No ${guessLetter}'s!`) +
+          `\nWaiting for other player's turn...`,
+      );
+    }
 
-    // only sends notification to correct vendor
-    socket.to(payload.store).emit(eventPool[2], payload);
+    // Check if the game is over (all letters have been revealed)
+    let isGameOver = revealedWord.every(letter => letter !== '_');
+
+    if (isGameOver) {
+      // Determine the highscore and the winners of the game
+      let highscore = 0;
+      let winners = [];
+
+      playerQueue.players.forEach(player => {
+        if (player.score > highscore) {
+          highscore = player.score;
+          winners = [player];
+        } else if (player.score === highscore) {
+          winners.push(player);
+        }
+      });
+
+      // Create a comma-separated string of winner names
+      let winnerNames = winners.map(winner => winner.name).join(', ');
+
+      // Emit the game over message to the guessing player
+      socket.emit(
+        eventPool.eventPool.PLAYER_GUESS,
+        `GAME OVER!! Winners are ${winnerNames} with ${highscore} points!`,
+      );
+
+      // Broadcast the game over message to all players in the room
+      socket
+        .to(`gameRoom`)
+        .emit(
+          eventPool.eventPool.PLAYER_GUESS,
+          `GAME OVER!! Winners are ${winnerNames} with ${highscore} points!`,
+        );
+      return;
+    }
+
+    // Update the turn ID for the next player's turn
+    turnId++;
+    if (turnId > playerQueue.players.length) {
+      turnId = 1;
+    }
+
+    // Prepare the payload for the 'PLAYER_TURN' event
+    payload = {
+      revealedWord: revealedWord,
+      guessLetter: guessLetter,
+      previousPlayer: playerQueue.players[turnId - 1].name,
+      turnId: turnId,
+    };
+
+    // Emit the 'PLAYER_TURN' event to all players in the room except the guessing player
+    socket.to('gameRoom').emit(eventPool.eventPool.PLAYER_TURN, payload);
   });
 });
-
-// eventEmitter.on(eventPool[0], logEvent(eventPool[0]))
-// eventEmitter.on(eventPool[1], logEvent(eventPool[1]))
-// eventEmitter.on(eventPool[2], logEvent(eventPool[2]))
-
-// require('./driver/index')
-// require('./vendor/index')
